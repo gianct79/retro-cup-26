e3895b68bcdf80108ab9b3cf9c3729bf989bbe5a
Giancarlo Tomazelli <gianct79@gmail.com>
2026-06-10 20:13:29 -0400

    v2
    v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/retro-cup-26/',
   server: {
     port: 3000,
     open: true,
